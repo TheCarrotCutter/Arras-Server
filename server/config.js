91f5a8d310897d414685f4cbad69f82b03a54283
TheCarrotCutter <masonthedecker@gmail.com>
2025-06-29 15:45:35 -0400

    Update config.js

--- server/config.js
+++ server/config.js
@@ -3,7 +3,7 @@ module.exports = {
 
     // Game server domain.
     // If the host is 'localhost:NUMBER', the NUMBER must be the port setting.
-    host: "arras-server.onrender.com",
+    host: "localhost:26301",
 
     // Which port to run the web server on.
     port: 26301,
